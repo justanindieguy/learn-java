1399ccf4de1acd662a13deaf1ec2906f326d9307
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-03-04 14:35:57 -0600

    learn about wrapper classes for primitive types

---
+++ DataTypes/DataTypes.java
@@ -0,0 +1,29 @@
+public class DataTypes {
+  public static void main(String[] args) {
+    System.out.println("Integer Values:");
+    System.out.println("Int Min: " + Integer.MIN_VALUE);
+    System.out.println("Int Max: " + Integer.MAX_VALUE);
+    System.out.println("Int Bytes: " + Integer.BYTES);
+
+    System.out.println();
+
+    System.out.println("Byte Values:");
+    System.out.println("Byte Min: " + Byte.MIN_VALUE);
+    System.out.println("Byte Max: " + Byte.MAX_VALUE);
+    System.out.println("Byte Bytes: " + Byte.BYTES);
+
+    System.out.println();
+
+    System.out.println("Float Values:");
+    System.out.println("Float Min: " + Float.MIN_VALUE);
+    System.out.println("Float Max: " + Float.MAX_VALUE);
+    System.out.println("Float Bytes: " + Float.BYTES);
+
+    System.out.println();
+
+    System.out.println("Character Values:");
+    System.out.println("Character Min: " + Character.MIN_VALUE);
+    System.out.println("Character Max: " + Character.MAX_VALUE);
+    System.out.println("Character Bytes: " + Character.BYTES);
+  }
+}
