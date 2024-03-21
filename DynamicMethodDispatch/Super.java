1859c7d418e22bee466936141d88df4d5a1f18fd
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-03-21 12:23:43 -0600

    implement super class

---
+++ DynamicMethodDispatch/Super.java
@@ -0,0 +1,12 @@
+/**
+ * Super
+ */
+public class Super {
+  public void methodOne() {
+    System.out.println("Method 1");
+  }
+
+  public void methodTwo() {
+    System.out.println("Method 2");
+  }
+}
