00741519598f32cda17cc3add2eb1b02936ec388
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-04-02 10:45:24 -0600

    refactor challenge to use SequenceInputStream

--- InputOutputStreams/ChallengeMergeFileContents/Main.java
+++ InputOutputStreams/ChallengeMergeFileContents/Main.java
@@ -1,57 +1,35 @@
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.SequenceInputStream;
 
 public class Main {
   public static void main(String[] args) {
-    String sourceOne = readFileContents("Source1.txt");
-    String sourceTwo = readFileContents("Source2.txt");
-
-    String fileName = "Destination.txt";
-    writeFile(fileName, sourceOne, sourceTwo);
+    mergeFiles("Source1.txt", "Source2.txt", "Destination.txt");
   }
 
-  private static String readFileContents(String path) {
+  private static void mergeFiles(String fileOnePath, String fileTwoPath, String fileName) {
     try {
-      FileReader fr = new FileReader(path);
+      FileInputStream fis1 = new FileInputStream(fileOnePath);
+      FileInputStream fis2 = new FileInputStream(fileTwoPath);
+      SequenceInputStream sis = new SequenceInputStream(fis1, fis2);
+
+      FileOutputStream fos = new FileOutputStream(fileName);
 
       int x;
-      StringBuilder sb = new StringBuilder();
-      while ((x = fr.read()) != -1) {
-        sb.append((char) x);
+      while ((x = sis.read()) != -1) {
+        fos.write(x);
       }
 
-      fr.close();
-      return sb.toString();
+      fis1.close();
+      fis2.close();
+      fos.close();
+      sis.close();
     } catch (FileNotFoundException e) {
       System.out.println(e);
-      return "";
-    } catch (IOException e) {
-      System.out.println(e);
-      return "";
-    }
-  }
-
-  private static void writeFile(String fileName, String... contents) {
-    try {
-      FileWriter fw = new FileWriter(fileName);
-
-      for (String str : contents) {
-        fw.write(addLineBreakIfNeeded(str));
-      }
-
-      fw.close();
     } catch (IOException e) {
       System.out.println(e);
     }
-  }
-
-  private static String addLineBreakIfNeeded(String str) {
-    if (str.charAt(str.length() - 1) != '\n') {
-      return str + '\n';
-    }
-
-    return str;
   }
 }
